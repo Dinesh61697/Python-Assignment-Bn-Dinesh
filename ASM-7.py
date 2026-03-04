55fa0955e127458ddebd8e347e0e0406f70fe105
Dinesh61697 <144544493+Dinesh61697@users.noreply.github.com>
2026-03-04 08:36:19 +0530

    tasks

---
+++ ASM-7.py
@@ -0,0 +1,53 @@
+
+Day-7:
+
+#1.Ordered: 
+
+elements=(10,20,30,40,50,60,70,80,90)
+print("elements",elements)
+                            
+
+
+#2.Immutable: 
+
+##elements=(1,2,3)
+##elements(0)=100
+##print(elements)
+                              
+
+
+#3.Allow Duplicates: 
+
+elements=(10,20,30,10)
+print(elements)
+                            
+
+
+
+#4.It stores different data types
+
+elements=(10,20,"Dinesh",11.9,"true")
+print(elements)
+                             
+
+
+#5. slicing : 
+
+elements=(10,20,30,40,50)
+print('elements[0:4]',elements)
+                            
+ 
+
+ #6.Append :
+
+elements.append(60)
+print("after append",elements)
+                              
+
+ 
+
+ #7.Insert : 
+
+elements.insert(25)
+print('after insert',elements)
+                              
