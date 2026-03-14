009fab2064ae7ed7157d5ef0a8ee498172f675e9
Dinesh61697 <144544493+Dinesh61697@users.noreply.github.com>
2026-03-14 19:25:20 +0530

    Daily tasks

---
+++ ASM-13.py
@@ -0,0 +1,73 @@
+Question no:1
+if else:
+
+num = int(input("enter a number: "))
+
+if num % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
+
+Question no:2
+while loop:
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i=i+1
+
+
+Question no:3
+for loop:
+
+for i in range(1,10):
+    print(i)
+
+
+Question no:4
+class and objects:
+
+class shapes:
+
+    def triangle(self):
+        print("triangle is a 15 cm")
+
+a = shapes()
+a.triangle()
+
+
+Question no:5
+inheritance:
+
+class A:
+    def show(self):
+        print("This is class A")
+
+class B(A):
+    def display(self):
+        print("This is class B")
+
+obj = B()
+
+obj.show()
+obj.display()
+
+
+Question:6
+lists:
+
+names = ["Dinesh","Santhosh","Anbu","Jeeva","Jawahar"]
+
+for n in names:
+    print(n)
+
+
+Question no:7
+Tuple:
+
+Games = ("Pubg", "Free fire", "clash of clans")
+
+for G in Games:
+    print(G)
+##
